b6d64520383985a79a5ec79d4c161d7bbc3da41d
Victor <luanvitorlima2017@gmail.com>
2023-10-24 06:20:54 -0300

    inicio do portfolio com react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react';
+import logo from './imagens/Logo.png';
+import { Instagram, Linkedin, Github } from 'react-bootstrap-icons';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <img src={logo} alt='logomarca'/>
+        <li><a href='#Home'>Home</a></li>
+        <li><a href='#Projects'>Projects</a></li>
+        <li><a href='#Contact'>Contact</a></li>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <main>
+        <section id='Home'>
+        <h2>L. Victor</h2>
+          <p>Software Enginner</p>
+          <div className='social'>
+            <Instagram/>
+            <Linkedin/>
+            <Github/>
+          </div>
+        </section>
+        <section id='Projects'>
+          Projects
+        </section>
+        <section id='Contact'>Contact</section>
+        </main>
       </header>
     </div>
   );
